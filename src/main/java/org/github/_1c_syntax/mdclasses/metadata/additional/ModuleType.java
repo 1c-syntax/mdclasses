7487369952649dacc8b902ae3879f7bd898852b2
Tymko Oleg <olegtymko@yandex.ru>
2019-07-26 22:35:12 +0700

    + типы модулей

---
+++ src/main/java/org/github/_1c_syntax/mdclasses/metadata/additional/ModuleType.java
@@ -0,0 +1,16 @@
+package org.github._1c_syntax.mdclasses.metadata.additional;
+
+public enum ModuleType {
+
+    CommandModule,
+    ObjectModule,
+    ManagerModule,
+    RecordSetModule,
+    ValueManagerModule,
+    ApplicationModule,
+    ManagedApplicationModule,
+    SessionModule,
+    ExternalConnectionModule,
+    OrdinaryApplicationModule
+
+}
