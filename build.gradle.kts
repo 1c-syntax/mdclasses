ed5e43dbf8278e02393b8c4c9c2f10a6747a91e8
Maxmov Valery <maximovvalery@gmail.com>
2020-01-06 13:40:19 +0300

    fix bump gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,22 +17,23 @@ repositories {
     maven { url = URI("https://jitpack.io") }
 }
 
+val junitVersion = "5.5.2"
 dependencies {
 
-    compile("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.9.4")
+    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-xml", "2.9.4")
 
     // логирование
-    compile("org.slf4j", "slf4j-api", "1.8.0-beta4")
-    compile("org.slf4j", "slf4j-simple", "1.8.0-beta4")
+    implementation("org.slf4j", "slf4j-api", "1.8.0-beta4")
+    implementation("org.slf4j", "slf4j-simple", "1.8.0-beta4")
     // прочее
-    compile("commons-io", "commons-io", "2.6")
-    compile("org.apache.commons", "commons-lang3", "3.9")
+    implementation("commons-io", "commons-io", "2.6")
+    implementation("org.apache.commons", "commons-lang3", "3.9")
     // генерики
     compileOnly("org.projectlombok", "lombok", lombok.version)
     // тестирование
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.5.0")
-    testRuntime("org.junit.jupiter", "junit-jupiter-engine", "5.5.0")
-    testCompile("org.assertj", "assertj-core", "3.12.2")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", junitVersion)
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", junitVersion)
+    testImplementation("org.assertj", "assertj-core", "3.12.2")
     testImplementation("com.ginsberg", "junit5-system-exit", "1.0.0")
 }
 
