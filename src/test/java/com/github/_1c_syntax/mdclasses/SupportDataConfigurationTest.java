45fadfc077a4a8cdc60dd5fce32d8f3e6e834f6c
otymko <olegtymko@yandex.ru>
2020-01-10 11:03:53 +0700

    + тесты

--- src/test/java/com/github/_1c_syntax/mdclasses/SupportDataConfigurationTest.java
+++ src/test/java/com/github/_1c_syntax/mdclasses/SupportDataConfigurationTest.java
@@ -1,9 +1,12 @@
 package com.github._1c_syntax.mdclasses;
 
+import com.github._1c_syntax.mdclasses.metadata.Configuration;
 import com.github._1c_syntax.mdclasses.metadata.SupportDataConfiguration;
+import com.github._1c_syntax.mdclasses.metadata.additional.SupportVariant;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,6 +18,47 @@ public class SupportDataConfigurationTest {
   public void testRead() {
     SupportDataConfiguration supportDataConfiguration = new SupportDataConfiguration(parentConfigurationsBin.toPath());
     assertThat(supportDataConfiguration.getSupportMap().size()).isNotZero();
+  }
+
+  @Test
+  void testConfigurationSupportEDT() {
+
+    final var PATH_TO_SUPPORT = "src/test/resources/support/edt";
+    var srcPath = new File(PATH_TO_SUPPORT);
+    var configuration = Configuration.newBuilder(srcPath.toPath()).build();
+
+    assertThat(configuration.getModulesBySupport().size()).isNotZero();
+
+    var path = Paths.get(PATH_TO_SUPPORT, "src/Catalogs/ПервыйСправочник/ObjectModule.bsl").toAbsolutePath();
+    assertThat(configuration.getModuleSupport(path.toUri()).containsValue(SupportVariant.NOT_EDITABLE)).isTrue();
+
+    path = Paths.get(PATH_TO_SUPPORT, "src/Configuration/SessionModule.bsl").toAbsolutePath();
+    assertThat(configuration.getModuleSupport(path.toUri()).containsValue(SupportVariant.SAVED)).isTrue();
+
+    path = Paths.get(PATH_TO_SUPPORT, "src/Documents/ПервыйДокумент/ObjectModule.bsl").toAbsolutePath();
+    assertThat(configuration.getModuleSupport(path.toUri()).containsValue(SupportVariant.OFF)).isTrue();
+
+  }
+
+  @Test
+  void testConfigurationSupportDesigner() {
+
+    final var PATH_TO_SUPPORT = "src/test/resources/support/original";
+    var srcPath = new File(PATH_TO_SUPPORT);
+    var configuration = Configuration.newBuilder(srcPath.toPath()).build();
+
+    // пока просто проверим что там чтото есть
+    assertThat(configuration.getModulesBySupport().size()).isNotZero();
+
+    var path = Paths.get(PATH_TO_SUPPORT, "Catalogs/ПервыйСправочник/Ext/ObjectModule.bsl").toAbsolutePath();
+    assertThat(configuration.getModuleSupport(path.toUri()).containsValue(SupportVariant.NOT_EDITABLE)).isTrue();
+
+    path = Paths.get(PATH_TO_SUPPORT, "Ext/SessionModule.bsl").toAbsolutePath();
+    assertThat(configuration.getModuleSupport(path.toUri()).containsValue(SupportVariant.SAVED)).isTrue();
+
+    path = Paths.get(PATH_TO_SUPPORT, "Documents/ПервыйДокумент/Ext/ObjectModule.bsl").toAbsolutePath();
+    assertThat(configuration.getModuleSupport(path.toUri()).containsValue(SupportVariant.OFF)).isTrue();
+
   }
 
 }
