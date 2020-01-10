34f785750905b9a00d399cfba6f769a13c48e04f
otymko <olegtymko@yandex.ru>
2020-01-10 11:10:36 +0700

    упрощение кода с ломбок

--- src/main/java/com/github/_1c_syntax/mdclasses/metadata/SupportConfiguration.java
+++ src/main/java/com/github/_1c_syntax/mdclasses/metadata/SupportConfiguration.java
@@ -1,17 +1,12 @@
 package com.github._1c_syntax.mdclasses.metadata;
 
-  import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Value;
 
 @Value
+@AllArgsConstructor
 public class SupportConfiguration {
   private String name;
   private String provider;
   private String version;
-
-  public SupportConfiguration(String name, String provider, String version) {
-    this.name = name;
-    this.provider = provider;
-    this.version = version;
-  }
-
 }
