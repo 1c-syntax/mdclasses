37b807f92f863b526ee133eb80ead0d70b8324be
Tymko Oleg <olegtymko@yandex.ru>
2020-10-13 22:53:13 +0700

    #111 / Тесты на чтение данных формы

--- src/test/java/com/github/_1c_syntax/mdclasses/mdo/FormDataTest.java
+++ src/test/java/com/github/_1c_syntax/mdclasses/mdo/FormDataTest.java
@@ -21,11 +21,12 @@
  */
 package com.github._1c_syntax.mdclasses.mdo;
 
-import com.github._1c_syntax.mdclasses.mdo.wrapper.DesignerForm;
+import com.github._1c_syntax.mdclasses.mdo.wrapper.form.DesignerForm;
 import com.github._1c_syntax.mdclasses.unmarshal.XStreamFactory;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,7 +34,7 @@ class FormDataTest {
 
   @Test
   void testEDT() {
-    var path = Path.of("src/test/resources/metadata/edt/src/Catalogs/Справочник1/Forms/ФормаСписка/Form.form");
+    var path = Path.of("src/test/resources/metadata/formdata/edt/ЖурналРегистрации/Form.form");
     FormData formDataEDT = (FormData) XStreamFactory.fromXML(path.toFile());
     formDataEDT.fillPlainChildren(formDataEDT.getChildren());
     checkFormData(formDataEDT);
@@ -41,14 +42,34 @@ class FormDataTest {
 
   @Test
   void testDesigner() {
-    var path = Path.of("src/test/resources/metadata/original/Catalogs/Справочник1/Forms/ФормаСписка/Ext/Form.xml");
+    var path = Path.of("src/test/resources/metadata/formdata/original/ЖурналРегистрации/Ext/Form.xml");
     var designerForm = (DesignerForm) XStreamFactory.fromXML(path.toFile());
     FormData formDataOrigin = new FormData(designerForm);
     checkFormData(formDataOrigin);
   }
 
   private void checkFormData(FormData formData) {
-    assertThat(formData.getPlainChildren()).hasSize(34);
+    assertThat(formData.getPlainChildren())
+      .hasSize(127)
+      .anyMatch(formItem -> formItem.getName().equals("ГруппаОтбора") && formItem.getId() == 103);
+
+    var findEmptyType = formData.getPlainChildren().stream()
+      .anyMatch(formItem -> formItem.getType().isEmpty());
+    assertThat(findEmptyType).isFalse();
+
+    assertThat(formData.getChildren())
+      .hasSize(3)
+      .anyMatch(formItem -> formItem.getName().equals("ГруппаОтбора")
+        && formItem.getId() == 103
+        && formItem.getChildren().size() == 4);
+
+    assertThat(formData.getHandlers())
+      .hasSize(3)
+      .anyMatch(handler -> handler.getEvent().equals("ChoiceProcessing")
+        && handler.getName().equals("ОбработкаВыбора"));
+
+    assertThat(formData.getAttributes()).hasSize(12)
+      .anyMatch(attribute -> attribute.getName().equals("Объект") && attribute.getId() == 1 && attribute.isMain());
   }
 
 }
