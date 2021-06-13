7c258ea981cb7c12cc83ab92420c66ec798b9ffc
Tymko Oleg <olegtymko@yandex.ru>
2021-06-13 23:38:33 +0700

    feat (model): #230 / Чтение режима пароля в константах, общий реквизитах. А так же в измерениях, реквизитах, ресурсах

--- src/test/java/com/github/_1c_syntax/mdclasses/mdo/PasswordModeTest.java
+++ src/test/java/com/github/_1c_syntax/mdclasses/mdo/PasswordModeTest.java
@@ -25,6 +25,7 @@ import com.github._1c_syntax.mdclasses.Configuration;
 import com.github._1c_syntax.mdclasses.mdo.attributes.AbstractMDOAttribute;
 import com.github._1c_syntax.mdclasses.mdo.attributes.TabularSection;
 import com.github._1c_syntax.mdclasses.mdo.metadata.AttributeType;
+import com.github._1c_syntax.mdclasses.mdo.support.MDOType;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
@@ -49,6 +50,76 @@ class PasswordModeTest {
   }
 
   private void performCheck(Configuration configuration) {
+    checkCatalog(configuration);
+    checkConstant(configuration);
+    checkCommonAttribute(configuration);
+    checkInformationRegister(configuration);
+  }
+
+  private void checkInformationRegister(Configuration configuration) {
+    var optionalInformationRegister = configuration.getChildren().stream()
+      .filter(abstractMDObjectBase -> abstractMDObjectBase.getType() == MDOType.INFORMATION_REGISTER
+        && abstractMDObjectBase.getName().equals("КакойТоРегистр"))
+      .findAny();
+    assertThat(optionalInformationRegister).isPresent();
+
+    var register = (MDInformationRegister) optionalInformationRegister.get();
+
+    assertThat(register.getAttributes())
+      .hasSize(6)
+      .anyMatch(attribute -> passedCheckAttribute(attribute,
+        AttributeType.DIMENSION, "Реквизит1", false))
+      .anyMatch(attribute -> passedCheckAttribute(attribute,
+        AttributeType.DIMENSION, "Пароль1", true))
+      .anyMatch(attribute -> passedCheckAttribute(attribute,
+        AttributeType.RESOURCE, "Пароль2", true))
+      .anyMatch(attribute -> passedCheckAttribute(attribute,
+        AttributeType.RESOURCE, "Реквизит2", false))
+      .anyMatch(attribute -> passedCheckAttribute(attribute,
+        AttributeType.ATTRIBUTE, "Пароль3", true))
+      .anyMatch(attribute -> passedCheckAttribute(attribute,
+        AttributeType.ATTRIBUTE, "Реквизит3", false));
+  }
+
+  private boolean passedCheckAttribute(AbstractMDOAttribute attribute, AttributeType type, String name,
+                                       boolean passwordMode) {
+    return attribute.getAttributeType() == type
+      && attribute.getName().equals(name)
+      && attribute.isPasswordMode() == passwordMode;
+  }
+
+  private void checkConstant(Configuration configuration) {
+    var optionalConstant = configuration.getChildren().stream()
+      .filter(abstractMDObjectBase -> abstractMDObjectBase.getType() == MDOType.CONSTANT
+        && abstractMDObjectBase.getName().equals("Пароль"))
+      .findAny();
+    assertThat(optionalConstant).isPresent();
+
+    var constant = (MDConstant) optionalConstant.get();
+    assertThat(constant.isPasswordMode()).isTrue();
+  }
+
+  private void checkCommonAttribute(Configuration configuration) {
+    var optionalCommonAttribute = configuration.getChildren().stream()
+      .filter(abstractMDObjectBase -> abstractMDObjectBase.getType() == MDOType.COMMON_ATTRIBUTE
+        && abstractMDObjectBase.getName().equals("ОбщийРеквизит1"))
+      .findAny();
+    assertThat(optionalCommonAttribute).isPresent();
+
+    var commonAttribute = (MDCommonAttribute) optionalCommonAttribute.get();
+    assertThat(commonAttribute.isPasswordMode()).isTrue();
+  }
+
+  private boolean validTabularSection(AbstractMDOAttribute mdoAttribute) {
+    var tabularSection = (TabularSection) mdoAttribute;
+    assertThat(tabularSection.getAttributes())
+      .hasSize(2)
+      .anyMatch(attribute -> attribute.getName().equals("Пароль") && attribute.isPasswordMode())
+      .anyMatch(attribute -> attribute.getName().equals("Реквизит1") && !attribute.isPasswordMode());
+    return true;
+  }
+
+  private void checkCatalog(Configuration configuration) {
     var optionalCatalog = configuration.getChildren().stream()
       .filter(abstractMDObjectBase -> abstractMDObjectBase instanceof MDCatalog)
       .findAny();
@@ -59,16 +130,8 @@ class PasswordModeTest {
     assertThat(catalog.getAttributes())
       .hasSize(2)
       .anyMatch(attribute -> attribute.getAttributeType() == AttributeType.ATTRIBUTE && attribute.isPasswordMode())
-      .anyMatch(attribute -> attribute.getAttributeType() == AttributeType.TABULAR_SECTION && validTabularSection(attribute));
-  }
-
-  private boolean validTabularSection(AbstractMDOAttribute mdoAttribute) {
-    var tabularSection = (TabularSection) mdoAttribute;
-    assertThat(tabularSection.getAttributes())
-      .hasSize(2)
-      .anyMatch(attribute -> attribute.getName().equals("Пароль") && attribute.isPasswordMode())
-      .anyMatch(attribute -> attribute.getName().equals("Реквизит1") && !attribute.isPasswordMode());
-    return true;
+      .anyMatch(attribute ->
+        attribute.getAttributeType() == AttributeType.TABULAR_SECTION && validTabularSection(attribute));
   }
 
 }
