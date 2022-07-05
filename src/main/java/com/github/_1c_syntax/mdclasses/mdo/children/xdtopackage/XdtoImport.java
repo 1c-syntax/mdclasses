36277141b0e2051d54d2f72bd4ca7d352a665d21
theshadowco <maximovvalery@gmail.com>
2022-07-05 13:41:05 +0300

    1. bump gradle versio -> 7.4.2 2. change license plugin 3. update license (windows must die) 4. bump dependences version

--- src/main/java/com/github/_1c_syntax/mdclasses/mdo/children/xdtopackage/XdtoImport.java
+++ src/main/java/com/github/_1c_syntax/mdclasses/mdo/children/xdtopackage/XdtoImport.java
@@ -1,7 +1,7 @@
 /*
  * This file is a part of MDClasses.
  *
- * Copyright © 2019 - 2022
+ * Copyright (c) 2019 - 2022
  * Tymko Oleg <olegtymko@yandex.ru>, Maximov Valery <maximovvalery@gmail.com> and contributors
  *
  * SPDX-License-Identifier: LGPL-3.0-or-later
