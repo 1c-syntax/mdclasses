6146291e0d0e51b22d7cb9629ff302603d8a79d1
theshadowco <maximovvalery@gmail.com>
2023-10-08 09:17:43 +0300

    move build to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import me.qoomon.gitversioning.commons.GitRefType
 import java.net.URI
 import java.util.*
 
@@ -5,16 +6,34 @@ plugins {
     `java-library`
     `maven-publish`
     jacoco
+    signing
     id("org.cadixdev.licenser") version "0.6.1"
-    id("com.github.gradle-git-version-calculator") version "1.1.0"
+    id("me.qoomon.git-versioning") version "6.4.2"
+    id("com.gorylenko.gradle-git-properties") version "2.4.1"
     id("io.freefair.lombok") version "8.3"
     id("io.freefair.javadoc-links") version "8.3"
     id("io.freefair.javadoc-utf-8") version "8.3"
     id("org.sonarqube") version "4.4.1.3373"
+    id("io.codearte.nexus-staging") version "0.30.0"
 }
 
-group = "com.github.1c-syntax"
-version = gitVersionCalculator.calculateVersion("v")
+group = "io.github.1c-syntax"
+gitVersioning.apply {
+    refs {
+        considerTagsOnBranches = true
+        tag("v(?<tagVersion>[0-9].*)") {
+            version = "\${ref.tagVersion}\${dirty}"
+        }
+        branch(".+") {
+            version = "\${ref}-\${commit.short}\${dirty}"
+        }
+    }
+
+    rev {
+        version = "\${commit.short}\${dirty}"
+    }
+}
+val isSnapshot = gitVersioning.gitVersionDetails.refType != GitRefType.TAG
 
 repositories {
     mavenLocal()
@@ -150,10 +169,41 @@ license {
     exclude("**/*.orig")
 }
 
+signing {
+    val signingInMemoryKey: String? by project      // env.ORG_GRADLE_PROJECT_signingInMemoryKey
+    val signingInMemoryPassword: String? by project // env.ORG_GRADLE_PROJECT_signingInMemoryPassword
+    if (signingInMemoryKey != null) {
+        useInMemoryPgpKeys(signingInMemoryKey, signingInMemoryPassword)
+        sign(publishing.publications)
+    }
+}
+
 publishing {
+    repositories {
+        maven {
+            name = "sonatype"
+            url = if (isSnapshot)
+                uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            else
+                uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            val sonatypeUsername: String? by project
+            val sonatypePassword: String? by project
+
+            credentials {
+                username = sonatypeUsername // ORG_GRADLE_PROJECT_sonatypeUsername
+                password = sonatypePassword // ORG_GRADLE_PROJECT_sonatypePassword
+            }
+        }
+    }
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+
+            if (isSnapshot && project.hasProperty("simplifyVersion")) {
+                version = findProperty("git.ref.slug") as String + "-SNAPSHOT"
+            }
+
             pom {
                 description.set("Metadata read/write library for Language 1C (BSL)")
                 url.set("https://github.com/1c-syntax/mdclasses")
@@ -190,6 +240,11 @@ publishing {
             }
         }
     }
+}
+
+nexusStaging {
+    serverUrl = "https://s01.oss.sonatype.org/service/local/"
+    stagingProfileId = "15bd88b4d17915" // ./gradlew getStagingProfile
 }
 
 tasks.register("precommit") {
