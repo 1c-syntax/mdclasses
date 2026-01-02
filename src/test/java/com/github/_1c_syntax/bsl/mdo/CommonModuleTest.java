e77a8b9265f93b888a71c614dde4510d9167b80f
Maximov Valery <maximovvalery@gmail.com>
2026-01-02 09:29:28 +0300

    happy new year

--- src/test/java/com/github/_1c_syntax/bsl/mdo/CommonModuleTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/mdo/CommonModuleTest.java
@@ -1,7 +1,7 @@
 /*
  * This file is a part of MDClasses.
  *
- * Copyright (c) 2019 - 2025
+ * Copyright (c) 2019 - 2026
  * Tymko Oleg <olegtymko@yandex.ru>, Maximov Valery <maximovvalery@gmail.com> and contributors
  *
  * SPDX-License-Identifier: LGPL-3.0-or-later
